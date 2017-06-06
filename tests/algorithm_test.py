df6e2688494c03c1d9162c4c5cc3a3bccf908ec2
lxmly <xmlyfly@gmail.com>
2017-06-06 20:57:40 +0800

    不全测试用例

--- tests/algorithm_test.py
+++ tests/algorithm_test.py
@@ -1,18 +1,36 @@
-import pytest
-
-from util.databuilder import DataBuilder
-from algorithm.itemcf import Itemcf
-from algorithm.slop_one import SlopOne
 import os
 
+from algorithm.mf.baseline import Baseline
+from util.databuilder import DataBuilder
+from algorithm.mf.als import ExplicitALS
+from algorithm.mf.svd import SVD
+from algorithm.mf.svdpp import SVDPlusPlus
+from algorithm.neighbor.slop_one import SlopOne
+from algorithm.neighbor.itemcf import Itemcf
+
 file_name = os.path.abspath("data/u.data")
-data_builder = DataBuilder(file_name, k_folds=7)
+data_builder = DataBuilder(file_name, k_folds=5, just_test_one=True)
+
 
 def test_itemcf():
     data_builder.rmse(Itemcf())
+
 
 def test_slopOne():
     data_builder.rmse(SlopOne())
 
 
+def test_baseline():
+    data_builder.rmse(Baseline())
 
+
+def test_svd():
+    data_builder.rmse(SVD())
+
+
+def test_svdpp():
+    data_builder.rmse(SVDPlusPlus())
+
+
+def test_als():
+    data_builder.rmse(ExplicitALS())
