68dd44425ef17b2e740fbfb4393f5e9415171561
lxmly <xmlyfly@gmail.com>
2017-06-06 21:01:10 +0800

    补全测试用例

--- tests/algorithm_test.py
+++ tests/algorithm_test.py
@@ -8,6 +8,7 @@ from algorithm.mf.svdpp import SVDPlusPlus
 from algorithm.neighbor.slop_one import SlopOne
 from algorithm.neighbor.itemcf import Itemcf
 
+
 file_name = os.path.abspath("data/u.data")
 data_builder = DataBuilder(file_name, k_folds=5, just_test_one=True)
 
