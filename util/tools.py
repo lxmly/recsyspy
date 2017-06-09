bae5a19f7ca85fcc5388d043f76d4e8999abf13b
lxmly <xmlyfly@gmail.com>
2017-06-09 12:52:17 +0800

    增加timer

---
+++ util/tools.py
@@ -0,0 +1,13 @@
+import time
+
+
+class Timer(object):
+    """time util
+    """
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
